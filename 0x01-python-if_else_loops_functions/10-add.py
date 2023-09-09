a16cc2679a0613013b748d1ea6d9713307f6fec5
Victoriaolu <victoriaoluchiamakoh@gmail.com>
2023-09-09 00:51:47 -0700

    10-add.py

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    if number < 0:
-        number = -number
-    print(number % 10, end="")
-    return(number % 10)
+def add(a, b):
+    return(a + b)
