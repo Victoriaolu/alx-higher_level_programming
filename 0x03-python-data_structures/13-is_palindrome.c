0085763731a18904b98bb58556d125918a2b304b
Victoriaolu <victoriaoluchiamakoh@gmail.com>
2023-09-12 18:02:02 -0700

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,72 +3,44 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+ * is_palindrome - checks if the singly linked list is a palindrome.
+ * Return:0 if not palindrome,1 if palindrome.
  */
-size_t print_listint(const listint_t *h)
+int is_palindrome(listint_t **head)
 {
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
+    listint_t *slow = *head, *fast = *head, *prev_slow = *head, *midnode = NULL;
+    listint_t *second_half, *prev_of_slow = *head;
+    int res = 1; // 1 means true, 0 means false
 
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
+    if (*head != NULL && (*head)->next != NULL) {
+        while (fast != NULL && fast->next != NULL) {
+            fast = fast->next->next;
+            prev_slow = slow;
+            slow = slow->next;
+        }
+
+        if (fast != NULL) {
+            midnode = slow;
+            slow = slow->next;
+        }
+
+        second_half = slow;
+        prev_slow->next = NULL;
+        reverse(&second_half);
+        res = compare(*head, second_half);
+        reverse(&second_half);
+
+        if (midnode != NULL) {
+            prev_slow->next = midnode;
+            midnode->next = second_half;
+        } else {
+            prev_slow->next = second_half;
+        }
     }
 
-    return (n);
+    return res;
 }
 
-/**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-    listint_t *new;
-    listint_t *current;
 
-    current = *head;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
-
-    new->n = n;
-    new->next = NULL;
-
-    if (*head == NULL)
-        *head = new;
-    else
-    {
-        while (current->next != NULL)
-            current = current->next;
-        current->next = new;
-    }
-
-    return (new);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-    listint_t *current;
-
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
-}
+ 
